8fb91bbe59315e31e44d9fd0547c4b9cb220afde
Илья Маликов <ilyakor.mostovoy@yandex.ru>
2019-04-08 19:01:29 +0300

    Updates

---
+++ precache-manifest.95b4425d0b508a3e966b3d85e5c123f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ecf7f1a63ddc982c817",
+    "url": "/test-task-react-redux/static/js/runtime~main.6988acbe.js"
+  },
+  {
+    "revision": "9b35437dd90f5a9ea781",
+    "url": "/test-task-react-redux/static/js/main.c2f8eb0d.chunk.js"
+  },
+  {
+    "revision": "8969f6c9497da12c32cc",
+    "url": "/test-task-react-redux/static/js/2.a7bb522f.chunk.js"
+  },
+  {
+    "revision": "9b35437dd90f5a9ea781",
+    "url": "/test-task-react-redux/static/css/main.94e1fcba.chunk.css"
+  },
+  {
+    "revision": "8969f6c9497da12c32cc",
+    "url": "/test-task-react-redux/static/css/2.1938e6a5.chunk.css"
+  },
+  {
+    "revision": "24151ec1e8188cae79d0d990c40a7f4b",
+    "url": "/test-task-react-redux/index.html"
+  }
+];
